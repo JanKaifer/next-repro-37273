1618d6d2b5dc46d9f376d0b8c79e3b96cb75e6f3
Jan Kaifer <jan@kaifer.cz>
2023-01-17 10:07:58 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <Image src="/logo with space.png" alt={""} width={100} height={100} />;
 }
